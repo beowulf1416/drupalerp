bcd598998f4ef829df8b5eab53e9db7c94a51f8d
Ferdinand L. Tomale <beowulf1416@gmail.com>
2011-08-16 15:28:47 +0800

    added schema files

--- erp/web/sites/all/modules/erp/erp_client/db/mysql.sql
+++ erp/web/sites/all/modules/erp/erp_client/db/mysql.sql
@@ -1,11 +1,13 @@
-create table erp_clients if not exists (
+/*
+create table erp_clients (
 	id int auto_increment not null comment 'pk, client id',
 	name varchar(500) not null,
 	address varchar(1000) null,
 	description varchar(4000) null,
-	enabled int not null default 0 comment '0 = enabled, otherwise false'
+	enabled int not null default 0 comment '0 = enabled, otherwise false',
 	created date not null,
 	primary key (id)
 )
 engine innodb
 comment 'clients information';
+*/
