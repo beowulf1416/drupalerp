2d67bd3345dcdc142bb2485a530d6f3387003aeb
Ferdinand L. Tomale <beowulf1416@gmail.com>
2011-08-18 17:34:04 +0800

    modified erp contact module
    added contact load function. added theme function.
    fixed missing quotation on template file.

--- erp/web/sites/all/modules/erp/erp_client/erp-client-view.tpl.php
+++ erp/web/sites/all/modules/erp/erp_client/erp-client-view.tpl.php
@@ -1,4 +1,4 @@
 <div class="erp-client">
 	<h2 class="erp-client-name"><?php print($client->name); ?></h2>
-	<p class=erp-client-description"><?php print($client->description); ?></p>
+	<p class="erp-client-description"><?php print($client->description); ?></p>
 </div><!-- .erp-client -->
