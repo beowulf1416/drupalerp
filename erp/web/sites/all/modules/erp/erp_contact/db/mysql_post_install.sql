740d4158f38966bb1a41e8d756a4d56b1cdf1d30
Ferdinand Tomale <beowulf1416@gmail.com>
2011-08-20 08:16:37 -0400

    modified erp contact post install script

--- erp/web/sites/all/modules/erp/erp_contact/db/mysql_post_install.sql
+++ erp/web/sites/all/modules/erp/erp_contact/db/mysql_post_install.sql
@@ -1,1 +1,3 @@
-alter table erp_contacts add foreign key (client_id) references erp_clients(id);
+alter table erp_contacts 
+add foreign key (client_id) references erp_clients(id),
+add foreign key (uid) references users(uid);
